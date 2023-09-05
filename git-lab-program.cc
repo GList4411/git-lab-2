0764a007cad577b2aa73e92dafdaf51a977f5549
GList4411 <133797966+GList4411@users.noreply.github.com>
2023-09-05 17:20:45 -0400

    Create git-lab-program.cc
    First Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Greg List>
+ *        Date: <09/05/2023>
+ * Description: print basic output
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+  cout << "Hello Git!!" << endl;
+
+    return 0;
+}// main
